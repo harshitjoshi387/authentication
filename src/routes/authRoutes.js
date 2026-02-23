d521360b8a18715f4e3a03c739846eb645110808
Harshit joshi <harshjoshi25022002@gmail.com>
2026-02-23 18:46:38 +0530

    sage2

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,7 @@
  const express = require('express')
 
  const userModel = require('../model/user.model')
+ const jwt= require('jsonwebtoken')
  const authRouter= express.Router()
 
  /**
@@ -8,15 +9,34 @@
 
  */
 
+
  authRouter.post("/register",(req,res)=>{
     const {email,name, password}= req.body
+
+    const isUserAlreadyExists= userModel.findOne({email})
+
+    if (isUserAlreadyExists){
+        return res.status(409).json({
+            message:"user already exists with this email address"
+        })
+    }
+
+    
 
     const user = userModel.create({
         email,password, name
     })
+    const token = jwt.sign(
+       {
+        id:user._id,
+        email:user.email
+       }, 
+       process.env.JWT_SECRET
+    )
     res.status(201).json({
         message:"user registered successful",
-        user
+        user,
+        token
     })
  })
 
